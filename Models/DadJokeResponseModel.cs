e899d11b16ee9227d18ec64664c4f816cc7d9dcc
Saipronoti <saipronotimukherjee27@gmail.com>
2025-06-22 23:03:07 +0530

    joke size introduction

--- Models/DadJokeResponseModel.cs
+++ Models/DadJokeResponseModel.cs
@@ -1,9 +1,26 @@
 namespace degreed_assignment.Models
 {
+ public enum JokeSize
+    {
+        Short,
+        Medium,
+        Long
+    }
 public class DadJokeResponseModel
 {
         public string Id { get; set; }
         public string Joke { get; set; }
         public int Status { get; set; }
+
+         public JokeSize Size
+        {
+            get
+            {
+                var wordCount = Joke.Split(' ').Length;
+                if (wordCount < 10) return JokeSize.Short;
+                if (wordCount < 20) return JokeSize.Medium;
+                return JokeSize.Long;
+            }
+        }
 }
 }
